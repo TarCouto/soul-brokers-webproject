6def7e6966b2ad6ccc020f8610bc952f6a5abcdb
Tarcisio Couto <tarcisiocouto@MacBook-Air-de-Tarcisio.local>
2024-10-31 12:14:04 -0300

    Mensagem explicativa das alterações

--- src/components/news.tsx
+++ src/components/news.tsx
@@ -2,40 +2,42 @@ import Image from 'next/image'
 
 export default function News() {
   return (
-    <div className="bg-white text-black px-20  min-h-[881px] flex items-center justify-center">
-      <div className="">
+    <div className="relative bg-white text-black lgpx-20  min-h-[881px] flex items-center justify-center  max-w-[300px] lg:max-w-full">
+      <div className="pl-[40px]">
         <h2
-          className={`text-customGray text-[24px] font-medium font-clash-display mb-6 text-center `}
+          className={`text-customGray text-[24px] font-medium font-clash mb-6 text-center `}
         >
           Conheça
         </h2>
         <h1 className="text-3xl md:text-6xl font-bold mb-14 font-clash text-center">
           A tranquilidade que você precisa!
         </h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          <div className="relative md:h-auto max-w-[632px] max-h-[500px]">
-            <Image
-              src="/parking.svg"
-              alt="Gestão de Transportes"
-              layout="fill"
-              objectFit="cover"
-              className="w-full h-full"
-            />
-            <div className="absolute w-full bottom-0 left-0 p-4 bg-gradient-to-b from-gray-100/0 via-gray-800/50 to-black/70 z-10">
-              <div className="flex flex-col justify-end lg:h-56">
-                <h2 className={`text-lg font-bold mb-1 text-white font-clash`}>
-                  Gestão de Transportes
-                </h2>
-                <p className="text-base font-clash mb-4 text-white w-2/3">
-                  It was popularised in the 1960s with the release of Letraset
-                  sheets containing Lorem Ipsum passages, and more recently with
-                  desktop publishing software like Aldus PageMaker.
-                </p>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:pl-1 gap-4">
+          <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-2 ">
+            <div className="relative min-h-[300px] min-w-[200px] md:h-auto lg:max-w-[632px] lg:max-h-[500px] lg:col-span-2  lg:w-full">
+              <Image
+                src="/parking.svg"
+                alt="Gestão de Transportes"
+                layout="fill"
+                objectFit="cover"
+                className="w-full h-full"
+              />
+              <div className="absolute w-full lg:bottom-0 lg:left-0 lg:p-4 bg-gradient-to-b from-gray-100/0 via-gray-800/50 to-black/70 z-10">
+                <div className="flex flex-col justify-end lg:h-56">
+                  <h2 className="text-lg font-bold mb-1 text-white font-clash">
+                    Gestão de Transportes
+                  </h2>
+                  <p className="text-base font-clash mb-4 text-white w-2/3">
+                    It was popularised in the 1960s with the release of Letraset
+                    sheets containing Lorem Ipsum passages, and more recently
+                    with desktop publishing software like Aldus PageMaker.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
 
-          <div className="grid grid-rows-2 gap-2 ">
+          <div className="grid grid-rows-2 gap-2 lg:max-w-[632px]">
             <div className="flex flex-col md:flex-row max-w-[632px]">
               <div className="relative w-full md:w-3/4 min-h-[242px]">
                 <Image
